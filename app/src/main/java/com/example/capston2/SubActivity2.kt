ddd07c7d95529e99df94fbe137d089e3ba8beedf
jjh98 <wjdwnsgml98@naver.com>
2022-06-07 11:47:40 +0900

    인트로화면 + 전체 레이어 흐름 구성 완료

--- app/src/main/java/com/example/capston2/SubActivity2.kt
+++ app/src/main/java/com/example/capston2/SubActivity2.kt
@@ -13,7 +13,24 @@ class SubActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var intent = Intent(this, SubActivity3::class.java)
+        //보고서탭
+        var intent = Intent(this, SubActivity1::class.java)
         binding.btnStart.setOnClickListener { startActivity(intent) }
+
+        //타이머탭
+        //var intent1 = Intent(this, SubActivity2::class.java)
+        //binding.btnStart3.setOnClickListener { startActivity(intent1) }
+
+        //메인탭
+        var intent2 = Intent(this, MainActivity::class.java)
+        binding.btnStart4.setOnClickListener { startActivity(intent2) }
+
+        //동기부여탭
+        var intent3 = Intent(this, SubActivity3::class.java)
+        binding.btnStart5.setOnClickListener { startActivity(intent3) }
+
+        //설정탭
+        var intent4 = Intent(this, SubActivity4::class.java)
+        binding.btnStart6.setOnClickListener { startActivity(intent4) }
     }
 }
